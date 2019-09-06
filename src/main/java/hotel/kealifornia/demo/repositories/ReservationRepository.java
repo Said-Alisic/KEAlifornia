656287d693da924ce130ad8e074d50270efe30de
Jakub <staniojakub@gmail.com>
2019-09-06 18:33:24 +0200

    Add reservation site and styles

---
+++ src/main/java/hotel/kealifornia/demo/repositories/ReservationRepository.java
@@ -0,0 +1,16 @@
+package hotel.kealifornia.demo.repositories;
+
+import hotel.kealifornia.demo.models.Reservation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+//public class ReservationRepository implements IRepository<Reservation>{
+//
+//    @Autowired
+//    JdbcTemplate jdbcTemplate;
+//
+//
+//
+//}
