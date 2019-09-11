7c13b179799f4da77b38917c0a2c0552fb3766e6
Jakub <staniojakub@gmail.com>
2019-09-11 13:20:02 +0200

    Fetch rooms from Spring backend

--- frontend/src/pages/RoomsPage.js
+++ frontend/src/pages/RoomsPage.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const RoomsPage = props => {
+
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    fetchRooms();
+  }, [])
+
+  const fetchRooms = () => {
+    fetch('http://localhost:8080/rooms')
+      .then(res => {
+        return res.json();
+      })
+      .then(rooms => {
+        setRooms(rooms);
+      })
+  }
+
   return (
-    <div>
+    <div className='container mt-4'>
       <h1>Rooms Page</h1>
+      <ul>
+        {rooms.map(room => {
+          return <li>{room.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
